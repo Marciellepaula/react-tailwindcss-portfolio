4f1fdb1affd4af90cd5b0c4100fc7e51354e33e8
Marcielle Paula <marcielle0644@gmail.com>
2025-02-17 13:29:12 -0300

    ajustes

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,14 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+		bio: 'Sou uma desenvolvedora dedicada e apaixonada por tecnologia, sempre buscando soluções inovadoras e eficientes para otimizar sistemas e melhorar a experiência do usuário. Tenho experiência em desenvolvimento full stack, trabalhando tanto no backend quanto no frontend, garantindo a integração fluida entre diferentes serviços e tecnologias.',
 	},
 	{
 		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+		bio: 'Durante minha trajetória profissional, atuei na criação, manutenção e otimização de sistemas, aplicando boas práticas de desenvolvimento, como clean code, testes automatizados e refatoração constante, para garantir software de alta qualidade e escalabilidade. Tenho habilidades sólidas em frameworks modernos como Laravel, Vue.js e React.js.',
 	},
+	{
+	 	id: 3, 
+	 	bio: 'Atualmente, sigo aprimorando minhas habilidades e acompanhando as tendências do setor para continuar desenvolvendo soluções inovadoras e impactantes.'
+	}
 ];
